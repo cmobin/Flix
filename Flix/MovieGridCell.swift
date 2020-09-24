92153252ecb546f8cb6da3f07d93beaa1b3bc72d
Mobin <68511123+cmobin@users.noreply.github.com>
2020-09-23 20:16:16 -0400

    Assignment 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Mobin Uddin Chowdhury on 9/23/20.
+//  Copyright © 2020 MC. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
